e148b2308115d2d8961d01917602f892447dba9c
Racuzan-fem <ian.arribas2008@gmail.com>
2026-04-20 19:16:51 +0200

    bonus 1

--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -3,7 +3,7 @@ extends Node
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	print("nga")
+	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
