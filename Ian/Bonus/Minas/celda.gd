3c1f60eaae8e99095c3d7f1bff84de2f17c3ae7d
Racuzan-fem <ian.arribas2008@gmail.com>
2026-05-06 19:23:18 +0200

    mines

--- Ian/Bonus/Minas/celda.gd
+++ Ian/Bonus/Minas/celda.gd
@@ -1,25 +1,45 @@
 extends TextureButton
 
+signal cell_pressed(cell)
+
 var has_mine = false
 var revealed = false
 
-signal cell_pressed(cell)
+@onready var icon = $Sprite2D
 
 @onready var mine_texture = preload("res://Ian/Bonus/Minas/assets/bomb.png")
 @onready var safe_texture = preload("res://Ian/Bonus/Minas/assets/safe.png")
+@onready var default_texture = preload("res://Ian/Bonus/Plinko/Sprites/1 (1).jpg")
 
 func _ready():
-	self.texture_normal = null  # o textura base de la casilla
+	pressed.connect(_on_pressed)
+	texture_normal = default_texture
+	
+	icon.texture = null
+	icon.scale = Vector2(1, 1)
 
 func _on_pressed():
 	if revealed:
 		return
-
+	
 	revealed = true
 	emit_signal("cell_pressed", self)
 
 func reveal_mine():
-	texture_normal = mine_texture
+	icon.texture = mine_texture
+	animate_icon()
 
 func reveal_safe():
-	texture_normal = safe_texture
+	icon.texture = safe_texture
+	animate_icon()
+
+func animate_icon():
+	icon.scale = Vector2(0, 0)
+	var tween = create_tween()
+	tween.tween_property(icon, "scale", Vector2(0.4, 0.4), 0.2)
+
+func reset():
+	revealed = false
+	has_mine = false
+	icon.texture = null
+	texture_normal = default_texture
--- Ian/Bonus/Minas/minas.gd
+++ Ian/Bonus/Minas/minas.gd
@@ -9,14 +9,14 @@ var cell_scene = preload("res://Ian/Bonus/Minas/celda.tscn")
 
 var cells = []
 
-var multiplier = 1.5
-
+var multiplier = 1.0
 var game_over = false
 
 func _ready():
 	create_grid()
 	generate_mines()
 	update_ui()
+	button_cashout.pressed.connect(_on_button_cashout_pressed)
 
 func create_grid():
 	for i in range(25):  # 5x5
@@ -26,28 +26,30 @@ func create_grid():
 		cells.append(cell)
 		
 		cell.connect("cell_pressed", Callable(self, "on_cell_pressed"))
-	
+
 func generate_mines():
 	for cell in cells:
-		cell.has_mine = randf() < 0.2  # 20% minas
+		cell.has_mine = randf() < 0.2
 
-func on_cell_pressed(cell): 
+func on_cell_pressed(cell):
 	if game_over:
 		return
 
 	if cell.has_mine:
 		cell.reveal_mine()
+		lose()
 	else:
 		cell.reveal_safe()
-		
+		increase_multiplier()
+
 func increase_multiplier():
-	multiplier *= 2
+	multiplier *= 1.2
 	update_ui()
-	
+
 func lose():
 	game_over = true
 	multiplier = 0
-	label_status.text = "Has perdido"
+	label_status.text = "💥 Has perdido"
 	update_ui()
 
 func _on_button_cashout_pressed():
@@ -55,7 +57,18 @@ func _on_button_cashout_pressed():
 		return
 	
 	game_over = true
-	label_status.text = "Ganado: x" + str(round(multiplier * 100) / 100.0)
-	
+	label_status.text = "💰 Ganado: x" + str(round(multiplier * 100) / 100.0)
+
 func update_ui():
 	label_multiplier.text = "x" + str(round(multiplier * 100) / 100.0)
+
+func restart():
+	multiplier = 1.0
+	game_over = false
+	label_status.text = ""
+
+	for cell in cells:
+		cell.reset()
+
+	generate_mines()
+	update_ui()
